ee921fb4e08790d1d2f34517e0c932872a12e10a
Neuroborus <mihasso@protonmail.com>
2024-06-30 11:39:00 +0300

    outlines

---
+++ scripts/background.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+directory=~/conky-yasno
+
+get_pdf=$(curl -o $directory/yasno.pdf -f "$(cat $directory/.private_url.txt)")
+chmod=$(chmod 644 $directory/yasno.pdf)
+rm_png=$(rm -f $directory/yasno.png)
+create_png=$(pdftoppm -singlefile -png $directory/yasno.pdf $directory/yasno)
+
+# Define the directory path
+logs_path=$directory/tmp/logs
+# Create the directory if it doesn't exist
+mkdir -p "$logs_path"
+logs_path="$logs_path/background.log"
+
+# Write content to log file
+echo "*** Get pdf:" > "$logs_path"
+echo "$get_pdf" >> "$logs_path"
+echo "*** chmod:" >> "$logs_path"
+echo "$chmod" >> "$logs_path"
+echo "*** Remove png:" >> "$logs_path"
+echo "$rm_png" >> "$logs_path"
+echo "*** Create png:" >> "$logs_path"
+echo "$create_png" >> "$logs_path"
